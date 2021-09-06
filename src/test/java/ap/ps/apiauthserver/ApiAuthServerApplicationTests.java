f05ba002d453902a59302d60bfc0d0dab8e9cd8a
ahnwonyoung <wonyoung_ahn@tmax.co.kr>
2021-09-06 17:17:55 +0900

    초기커밋

---
+++ src/test/java/ap/ps/apiauthserver/ApiAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ap.ps.apiauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
